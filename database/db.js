b50c244db06de3169ae463a5cc0d411e23ae4833
unknown <r.hans4486@gmail.com>
2020-12-05 00:17:53 -0500

    Test Hositnger remote db

--- database/db.js
+++ database/db.js
@@ -7,11 +7,17 @@ const mysql = require('mysql');
 // 	database: 'modelize_db',
 // });
 
+// const connection = mysql.createConnection({
+// 	host: 'us-cdbr-east-02.cleardb.com',
+// 	user: 'ba3e368f04da2a',
+// 	password: '15162fd3',
+// 	// database: 'u191982321_api',
+// });
 const connection = mysql.createConnection({
-	host: 'us-cdbr-east-02.cleardb.com',
-	user: 'ba3e368f04da2a',
-	password: '15162fd3',
-	// database: 'u191982321_api',
+	host: 'sql220.main-hosting.eu',
+	user: 'u191982321_hans',
+	password: 'sBVxFIsBTk5:',
+	database: 'u191982321_modelize',
 });
 
 connection.connect(function (err) {
